372a43c1bd1a58438d6b15da5c689da1182bf7a3
nkurudavid <nkurudavid44@gmail.com>
2023-04-13 22:02:43 +0200

    created new project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <h1>This is an Home page</h1>
+    </div>
+  </main>
+</template>
