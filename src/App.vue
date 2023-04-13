16d85a75567e03e65a038cf2533d12f88c39b4cc
nkurudavid <nkurudavid44@gmail.com>
2023-04-14 00:32:11 +0200

    imported components on app

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
 
 </script>
 
 <template>
   <Navbar />
   <RouterView />
+  <Footer />
 </template>
