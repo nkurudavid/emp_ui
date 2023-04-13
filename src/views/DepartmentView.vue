2d8b1c297f0da51812d95d68973631234c1ecfb3
nkurudavid <nkurudavid44@gmail.com>
2023-04-13 22:11:17 +0200

    modified routing pages

--- src/views/DepartmentView.vue
+++ src/views/DepartmentView.vue
@@ -1,15 +1,11 @@
-<template>
-  <div class="container">
-    <h1>This is an about page</h1>
-  </div>
-</template>
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<template>
+  <main>
+    <div class="container">
+      <h1>This is an Department page</h1>
+    </div>
+  </main>
+</template>
---
+++ src/views/EmployeeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+// import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <h1>This is an Employment page</h1>
+    </div>
+  </main>
+</template>
