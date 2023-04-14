dd9ef4e0c4b3391e39583b0d2eebb7c23b8422b3
nkurudavid <nkurudavid44@gmail.com>
2023-04-14 18:16:30 +0200

    included font awesome icons

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-// import Vue from 'vue'
 
 import '../node_modules/axios/dist/axios.min'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import './assets/main.css'
 
-// import { library } from '@fortawesome/fontawesome-svg-core';
-// import { faHatWizard } from '@fortawesome/free-solid-svg-icons';
-// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-// library.add(faHatWizard);
-// Vue.component('font-awesome-icon', FontAwesomeIcon);
+library.add(fas, far, fab)
 
 
 const app = createApp(App)
+
+app.component('font-awesome-icon', FontAwesomeIcon)
 
 app.use(router)
 
--- src/views/DepartmentView.vue
+++ src/views/DepartmentView.vue
@@ -6,7 +6,10 @@ export default {
   name: 'Departments',
   data() {
     return {
-      departments:[]
+      departments: [],
+      modelTitle: "",
+      departmentName: "",
+      departmentId: "",
     }
   },
   mounted() {
@@ -14,7 +17,7 @@ export default {
   },
   methods: {
     getDepartments() {
-      axios.get(variables.API_URL+'departments').then(response => {
+      axios.get(variables.API_URL + 'departments').then(response => {
         this.departments = response.data
       }).catch(error => {
         console.error('Error fetching departments:', error)
@@ -40,18 +43,43 @@ export default {
               </tr>
             </thead>
             <tbody>
-              <tr v-for="dep in departments" :key="dep.id">
-                <th scope="row">{{ dep.id }}</th>
+              <tr v-for="dep, index in departments" :key="dep.id">
+                <th scope="row">{{ index + 1 }}</th>
                 <td>{{ dep.department }}</td>
                 <td>
-                  <a class="badge text-bg-primary" href="#">Edit</a>
-                  <a class="badge text-bg-danger" href="#">Delete</a>
+                  <a class="badge text-bg-primary p-2 m-1" href="#"><font-awesome-icon :icon="['fas', 'edit']" /> Edit</a>
+                  <a class="badge text-bg-danger p-2 m-1" href="#"><font-awesome-icon :icon="['fas', 'trash-can']" />
+                    Delete</a>
                 </td>
               </tr>
             </tbody>
           </table>
         </div>
       </div>
+
+      <!-- Button trigger modal -->
+      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+
+      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5 text-dark" id="exampleModalLabel">{{ modelTitle }}</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </div>
   </section>
 </template>
