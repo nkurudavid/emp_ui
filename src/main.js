6566f13f9d08b7af6b2d596bcc9955a6fa3a6734
nkurudavid <nkurudavid44@gmail.com>
2023-04-14 17:17:55 +0200

    installed font-awesame-icon

--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import Vue from 'vue'
 
 import '../node_modules/axios/dist/axios.min'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
 import './assets/main.css'
 
+// import { library } from '@fortawesome/fontawesome-svg-core';
+// import { faHatWizard } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// library.add(faHatWizard);
+// Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 
 const app = createApp(App)
